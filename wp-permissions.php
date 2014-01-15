8074cbb927ab7b32169c50837fff3b7113dd0db4
wpranger <dave@wpranger.co.uk>
2014-01-15 13:27:37 +0000

    re-wrote the file listing function completely, now uses a big Table

--- wp-permissions.php
+++ wp-permissions.php
@@ -4,7 +4,7 @@ Plugin Name: WordPress Upload Permissions
 Plugin URI: http://wpranger.co.uk
 Description: Lists the currently set WordPress uploads directory file permissions
 Author: Dave Naylor
-Version: 0.1
+Version: 0.2
 Author URI: http://wpranger.co.uk
 License: GPL2
 */
@@ -12,62 +12,119 @@ License: GPL2
 add_action('admin_init', 'wp_permissions_init' );
 add_action('admin_menu', 'wp_permissions_add_page');
 
-// Init plugin options to white list our options
 function wp_permissions_init(){
     register_setting( 'wp_permissions', 'wp-permissions', 'intval' );
 }
 add_action( 'admin_init', 'wp_permissions_init' );
 
-// Add menu page
 function wp_permissions_add_page() {
 	add_management_page('Upload Permissions', 'Upload Permissions', 'manage_options', 'wp_permissions', 'wp_permissions_tools_page');
 }
 
-// Draw the menu page itself
 function wp_permissions_tools_page() {
     $upload_dir =  wp_upload_dir();
-    $path = $upload_dir['basedir'];
+    $uploadpath = $upload_dir['basedir'];
+    $subdirpath = basename(realpath($uploadpath));
 	?>
+    <style type="text/css">
+    .file-permissions tr:nth-child(2n+1) {background: #ddd;}
+    .file-permissions tr:hover {background: #ffffee;}
+    .file-permissions td, .file-permissions th {padding: 10px;border: 1px solid #ccc;}
+    .file-permissions th {text-align: center; background: #ccc}
+    .file-permissions th.left {text-align: left;}
+    .file-permissions td.directory {color: green;}
+    .file-permissions td.file {color: blue;}
+    .file-permissions td.warn {background: salmon;}
+    </style>
 	<div class="wrap">
 		<h2>WordPress Upload Permissions</h2>
-        <h3>The currently set file and directory permissions for your WordPress uploads directory</h3>
-        <p><strong>Upload path is set to: </strong><?php echo $path; ?></p>
-        <p>Directories are shown in <span style="color: green;">Green</span> followed by their permission in octal notation (e.g. <span style="color: green;">755</span>)</p>
-        <p>Files are shown in <span style="color: blue;">Blue</span> followed by their permission in octal notation (e.g. <span style="color: blue;">644</span>)</p>
+        <h3>The currently set directory permissions for your WordPress uploads directory</h3>
+        <p><strong>Absolute upload path is set to: </strong><?php echo $uploadpath; ?></p>
+        <!-- <p>Directories are shown in <span style="color: green;">Green</span>, files are shown in <span style="color: blue;">Blue</span></p> -->
 	</div>
 	<?php	
-	    $tree = retrieveTree($path); 
-	    echo "<pre style='color:lightgray;'> ";
-	    print_r($tree);
-	    echo "</pre>";
-}
 
-$delim = strstr(PHP_OS, "WIN") ? "\\" : "/";
+    $dirlist = getFileList($uploadpath, true);
+    
+    echo "<table class='file-permissions'>\n";
+    echo "<tr><th class='left'>Name</th><th class='left'>Type</th><th>Permissions</th><th>Writeable</th><th>Readable</th></tr>\n";
+    foreach($dirlist as $file) {
+    echo "<tr>\n";
+    $findme = stripos($file['name'], $subdirpath);
+    $uppath = substr($file['name'],$findme);
 
-function retrieveTree($path)  {
-    global $delim;
-
-    if ($dir=@opendir($path)) {
-        while (($element=readdir($dir))!== false) {
-            if (is_dir($path.$delim.$element) && $element!= "." && $element!= "..") {
-			
-				$s = stat($path.$delim.$element);
-				$permissions = substr(decoct($s[mode]), -3);
-
-				$array_element = '<span style="color:green;">'.$element.' '.$permissions.'</span>';
-				
-				$array[$array_element] = retrieveTree($path.$delim.$element);
-				
-            } elseif ($element!= "." && $element!= "..") {
-			
-				$s = stat($path.$delim.$element);
-				$permissions = substr(decoct($s[mode]), -3);
-				
-                $array[] = '<span style="color:blue;">'.$element.' '.$permissions.'</span>';
-            }
-        }
-        closedir($dir);
+    if($file['type'] == "dir") {
+    echo "<td class='directory'>{$uppath}</td>\n";
+    } else { echo "<td class='file'>{$uppath}</td>\n";
     }
-    return (isset($array) ? $array : false);
+    echo "<td>{$file['type']}</td>\n";
+    echo "<td>{$file['fperm']}</td>\n";
+    if($file['write'] !== "Directory is writeable" && $file['type'] == "dir") {
+        echo "<td class='warn'>{$file['write']}</td>\n";
+    } 
+    else {  echo "<td>{$file['write']}</td>\n";
+    }
+    if($file['read'] !== "Directory is readable" && $file['type'] == "dir") {
+        echo "<td class='warn'>{$file['read']}</td>\n";
+    } 
+    else {  echo "<td>{$file['read']}</td>\n";
+    }
+    echo "</tr>\n";
+  }
+  echo "</table>\n\n";
 }
 
+function getFileList($dir, $recurse=false, $depth=false)
+  {
+    // array to hold return value
+    $retval = array();
+
+    // add trailing slash if missing
+    if(substr($dir, -1) != "/") $dir .= "/";
+
+    // open pointer to directory and read list of files
+    $d = @dir($dir) or die("getFileList: Failed opening directory $dir for reading");
+    while(false !== ($entry = $d->read())) {
+      // skip hidden files
+      if($entry[0] == ".") continue;
+      if(is_dir("$dir$entry")) {
+        if(is_writable("$dir$entry")) {
+            $wtest = "Directory is writeable";
+        } else {
+            $wtest = "Directory is <strong>NOT</strong> writeable";
+        }
+        if(is_readable("$dir$entry")) {
+            $rtest = "Directory is readable";
+        } else {
+            $rtest = "Directory is <strong>NOT</strong> readable";
+        }
+        $retval[] = array(
+          "name" => "$dir$entry/",
+          "type" => filetype("$dir$entry"),
+          "fperm" => decoct(fileperms("$dir$entry") & 0777),
+          "write" => "$wtest",
+          "read" => "$rtest"
+        );
+        if($recurse && is_readable("$dir$entry/")) {
+          if($depth === false) {
+            $retval = array_merge($retval, getFileList("$dir$entry/", true));
+          } elseif($depth > 0) {
+            $retval = array_merge($retval, getFileList("$dir$entry/", true, $depth-1));
+          }
+        }
+      } 
+
+      // Omitted Files for now until IO can implement ome kind of pagination
+     
+      //elseif(is_readable("$dir$entry")) {
+      //  $retval[] = array(
+      //    "name" => "$dir$entry",
+      //    "type" => mime_content_type("$dir$entry"),
+      //    "fperm" => decoct(fileperms("$dir$entry") & 0777) 
+      //  );
+      //}
+    }
+    $d->close();
+
+    return $retval;
+}
