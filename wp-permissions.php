f6e76ec921053b0fd017576309f99989b26b5087
wpranger <dave@wpranger.co.uk>
2014-01-16 16:19:22 +0000

    Added GPL License

--- wp-permissions.php
+++ wp-permissions.php
@@ -9,6 +9,22 @@ Author URI: http://wpranger.co.uk
 License: GPL2
 */
 
+/*  Copyright 2014  Dave Naylor  (email : dave@wpranger.co.uk)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 add_action('admin_init', 'wp_permissions_init' );
 add_action('admin_menu', 'wp_permissions_admin_menu');
 
